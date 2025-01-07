9c1f4eaadad92c7daeb9cfa49916cce78cff54b8
Francisco Moreno <fmoreno@aligntech.com>
2025-01-07 21:03:30 +0100

    feature: import tested file
    import file to test, also display dialog to find file

--- src/actions/initTest.ts
+++ src/actions/initTest.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import { dirname, relative } from 'node:path';
+
+const formatPath = (path: string) => path.includes('/') ? path : `./${path}`;
 
 export const initTest = async () => {
     const editor = vscode.window.activeTextEditor;
@@ -6,10 +9,19 @@ export const initTest = async () => {
         vscode.window.showErrorMessage('No active text editor');
         return;
     }
+    const targetFile = await vscode.window.showOpenDialog({
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: false
+    });
+    if (!targetFile || targetFile.length === 0) {
+        return;
+    }
     editor.edit(editBuilder => {
-        editBuilder.insert(editor.selection.active, 'import { strict } from \'esmock\';\n');
+        const pathTo = relative(dirname(vscode.window.activeTextEditor!.document.uri.fsPath), targetFile[0].fsPath);
+        editBuilder.insert(editor.selection.active, 'import esmock from \'esmock\';\n');
         editBuilder.insert(editor.selection.active, 'import assert from \'node:assert/strict\';\n');
         editBuilder.insert(editor.selection.active, 'import { describe, test } from \'node:test\';\n');
-        editBuilder.insert(editor.selection.active, 'describe(\'your first describe\', () =>{\ntest(\'your first test\', () =>{\nassert.equal(1, 1);});});');
+        editBuilder.insert(editor.selection.active, `describe(\'your first describe\', async() =>{\nconst f = await esmock.strict('${formatPath(pathTo)}', {}, {});\ntest(\'your first test\', () =>{\nassert.equal(1, 1);});});`);
     });
 };
