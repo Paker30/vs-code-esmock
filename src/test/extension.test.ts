be54dce586ac9bf14aa66afaaed9571494db774e
Francisco Moreno <fmoreno@aligntech.com>
2025-05-29 12:22:03 +0200

    test: cover generateCode
    check all known import cases

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,7 +4,7 @@ import sinon from 'sinon';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import { importedRegex, splitImportRegex, createTest } from '../actions/initTest';
+import { importedRegex, splitImportRegex, generateCode, writeTest } from '../actions/initTest';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -37,7 +37,23 @@ suite('Extension Test Suite', () => {
 		});
 	});
 
-	suite('createTest', () => {
+	suite('generateCode', () => {
+		const fakePath = 'utils';
+		test('import default', () => {
+			const [createdCode] = generateCode(fakePath)('import cheers from \'./utils.ts\';')!;
+			assert.equal(createdCode, "'../utils.ts': { default: mock.fn()}");
+		});
+		test('import with destructuring', () => {
+			const [createdCode] = generateCode(fakePath)("import { goodbye, greeter, Person } from './utils.ts';")!;
+			assert.equal(createdCode, "'../utils.ts': {  goodbye: mock.fn(), greeter: mock.fn(), Person : mock.fn()}");
+		});
+		test('import with default and destructuring', () => {
+			const [createdCode] = generateCode(fakePath)("import cheers, { hello, goodbye, greeter, Person } from './utils.ts';")!;
+			assert.equal(createdCode, "'../utils.ts': { default: mock.fn(), hello: mock.fn(), goodbye: mock.fn(), greeter: mock.fn(), Person : mock.fn()}");
+		});
+	});
+
+	suite('writeTest', () => {
 		const fakeInsert = sinon.stub();
 		const fakeEditBuilder = {
 			insert: fakeInsert
@@ -53,7 +69,7 @@ suite('Extension Test Suite', () => {
 		} as unknown as vscode.TextEditor;
 
 		test('check inserted code', () => {
-			createTest({ editor: fakeEditor as any, pathTo: '', mockedImports: [] })(fakeEditBuilder);
+			writeTest({ editor: fakeEditor as any, pathTo: '', mockedImports: [] })(fakeEditBuilder);
 			sinon.assert.callCount(fakeInsert, 4);
 		});
 
